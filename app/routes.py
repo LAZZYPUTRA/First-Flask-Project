905d95e254a473253f11106424c30dd29f47833d
LAZZYPUTRA <30putraramadhan@gmail.com>
2026-03-13 15:45:47 +0700

    test hello world

--- app/routes.py
+++ app/routes.py
@@ -1,2 +1,9 @@
 from flask import render_template
 from app import app
+@app.route("/")
+def home() :
+    hello = "hello world"
+    return render_template("index.html", hello=hello)
+
+if __name__ == "__main__" :
+    app.run(debug=True)
